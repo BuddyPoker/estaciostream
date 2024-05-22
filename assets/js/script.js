80b78c1a041a1ba6a51bb9c47ae6c0d5f89f8356
Alisson <140769066+alvesgc@users.noreply.github.com>
2024-05-21 22:11:25 -0300

    refactor: improving readability and encapsulating function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,19 @@ import { InsertMovies } from "./fetchMovies.js";
 export const apiKey = '05971feefd7f2a91f44479cbbbb4e21a';
 let movies = []; 
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`)
-  .then(response => response.json())
-  .then(data => {
-    movies = data.results.slice(0, 4); // Atribui o valor a movies, por exemplo (top 10 filmes populares)-
-    InsertMovies(movies);
-  })
-  .catch(error => {
-    console.error('Ocorreu um erro ao obter os filmes populares:', error);
-  })
+function fetchAndDisplayMovies(endpoint, numMovies) {
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      movies = data.results.slice(0, numMovies);
+      InsertMovies(movies);
+    })
+    .catch(error => {
+      console.error(`Ocorreu um erro ao obter os filmes em ${endpoint}:`, error);
+    });
+}
+
+const popularMoviesEndpoint = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`;
+const numPopularMovies = 4; // Altere este valor para o número desejado de filmes populares a serem exibidos
+
+fetchAndDisplayMovies(popularMoviesEndpoint, numPopularMovies);
